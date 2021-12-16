58c27266a76bba05bd6b7c8f669496892d862ed2
liushuai <1335387481@qq.com>
2021-12-16 17:45:01 +0800

    1

--- API-Page/Controllers/TestController.cs
+++ API-Page/Controllers/TestController.cs
@@ -1,11 +1,11 @@
-﻿using API_Page.data;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http.Cors;
+using API_Page.data;
+using API_Page.Models;
 using API_Page.newObj;
 using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json.Linq;
-using System;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Web.Http.Cors;
 
 namespace API_Page.Controllers
 {
@@ -15,9 +15,7 @@ namespace API_Page.Controllers
     [Produces("application/json")]
     public class TestController : ControllerBase
     {
-
-
-        private readonly ApiDBContent  _context;
+        private readonly ApiDBContent _context;
 
         public TestController(ApiDBContent myContext)
         {
@@ -27,18 +25,20 @@ namespace API_Page.Controllers
         //JsonResult
 
         [HttpPost("test")]
-        public IActionResult Test( userObj userObj)
+        public IActionResult Test(userObj userObj)
         {
             String username = userObj.username;
             String password = userObj.password;
-
-            var  user = _context.Users.ToList();
-            if (user==null)
+            
+            List<User> user = _context.Users.ToList();
+            if (user == null)
             {
-                return NotFound("没有相关数据");//找不到资源404状态码
-            };
+                return NotFound("没有相关数据"); //找不到资源404状态码
+            }
 
-            return Ok(user);//ControllerBase内定函数OK，携带相应数据，也代表200状态码
+            ;
+
+            return Ok(user); //ControllerBase内定函数OK，携带相应数据，也代表200状态码
         }
     }
 }
