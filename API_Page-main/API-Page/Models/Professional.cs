63cf3e88d761b9c0a910963f61094603529db6d5
liushuai <51101815+liushuai-user@users.noreply.github.com>
2021-12-17 15:10:16 +0800

    1
    1

--- API_Page-main/API-Page/Models/Professional.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace API_Page.Models
-{
-    public class Professional//专业表
-    {
-        public int Id { get; set; } //专业表ID
-
-        public string ProfessionalName { get; set; } //专业名称
-    }
-}
--- API_Page-main/API-Page/core/Enums/RoleEnum.cs
+++
@@ -1,7 +0,0 @@
-namespace API_Page.core.Enums;
-
-public enum RoleEnum
-{
-    STUDENT=0,
-    TEACHER=1
-}
